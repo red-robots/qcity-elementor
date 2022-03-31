8d85dfdaf95bc664fa9e59f1f7bb0f0815ddb59d
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-03-31 00:26:13 -0400

    Added business list success redirection

--- includes/extra.php
+++ includes/extra.php
@@ -73,6 +73,32 @@ function formatPhoneNumber($str) {
   }
 }
 
+function getFullURL($removeParam=null) {
+  $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+  if($removeParam) {
+    $actual_link = preg_replace('~(\?|&)'.$removeParam.'=[^&]*~', '$1', $actual_link);
+    $actual_link = rtrim($actual_link, "&");
+  }
+  return $actual_link;
+}
+
+function qct_my_custom_admin_head() { 
+$successURL = get_site_url() . '/listing-confirmation/'; ?>
+<script>
+var params={};location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){params[k]=v});
+jQuery(document).ready(function($){
+  if (window.location.href.indexOf("business-directory/add-business") > -1) {
+    if( $('form[name="geodirectory-add-post"]').length ) {
+      var redirectURI = '<input type="hidden" name="success_redirect" value="<?php echo $successURL; ?>">';
+      $('form[name="geodirectory-add-post"]').prepend(redirectURI);
+    }
+  }
+});
+</script>
+<?php
+}
+add_action( 'wp_footer', 'qct_my_custom_admin_head' );
+
 
 add_action('tribe_events_community_before_event_submission_page_template', function() {
   if ( isset( $_POST[ 'community-event' ] ) ) {
@@ -84,3 +110,26 @@ add_action('tribe_events_community_before_event_submission_page_template', funct
     }
   }
 } );
+
+
+function gd_snippet_ajax_save_post_message( $message, $post_data ) {
+  $redirect_to = ( isset($post_data['success_redirect']) && $post_data['success_redirect'] ) ? $post_data['success_redirect'] : ''; // Redirect url
+
+  ob_start();
+  if($redirect_to && $post_data['ID']) { 
+    //$message = '<strong>Redirecting...</strong>';
+    $message = '';
+  ?>
+  <script type="text/javascript">
+    document.getElementsByClassName('gd-notification')[0].style.display = 'none';
+    window.location = '<?php echo $redirect_to; ?>';
+  </script>
+  <?php 
+  $redirect_to_js = ob_get_contents();
+  ob_end_clean();
+  $message = trim($redirect_to_js);
+  return $message;
+  }
+}
+add_filter( 'geodir_ajax_save_post_message', 'gd_snippet_ajax_save_post_message', 100, 2 );
+
