1a8cb6a79ca1522c4859025b640d19252b78ae15
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-04-06 04:51:59 -0400

    modified event submission form

--- tribe-events/community/modules/organizer.php
+++ tribe-events/community/modules/organizer.php
@@ -21,53 +21,122 @@ defined( 'WPINC' ) or die;
 // If the user cannot create new organizers *and* if there are no organizers
 // to select from then there's no point in generating this UI
 if ( ! tribe( 'community.main' )->event_form()->should_show_linked_posts_module( Tribe__Events__Organizer::POSTTYPE ) ) {
-	return;
+  return;
 }
 
 if ( ! isset( $event ) ) {
-	$event = Tribe__Events__Main::postIdHelper();
+  $event = Tribe__Events__Main::postIdHelper();
 }
 ?>
 
 <div id="event_tribe_organizer" class="tribe-section tribe-section-organizer">
-	<div class="tribe-section-header">
-		<h3 class="<?php echo tribe_community_events_field_has_error( 'organizer' ) ? 'error' : ''; ?>">
-			<?php
-      printf( __( '%s', 'tribe-events-community' ), 'Organizer Contact Details' );
-			//printf( __( '%s Details', 'tribe-events-community' ), tribe_get_organizer_label_singular() );
-			echo tribe_community_required_field_marker( 'organizer' );
-			?>
-		</h3>
-	</div>
+  <div class="tribe-section-header">
+    <h3 class="<?php echo tribe_community_events_field_has_error( 'organizer' ) ? 'error' : ''; ?>">
+      <?php
+      printf( __( '%s', 'tribe-events-community' ), 'Organizer Contact Details ' );
+      //printf( __( '%s Details', 'tribe-events-community' ), tribe_get_organizer_label_singular() );
+      echo tribe_community_required_field_marker( 'organizer' );
+      ?>
+    </h3>
+  </div>
 
   <div class="field-note">This information will not be published.</div>
 
-	<?php
-	/**
-	 * Allow developers to hook and add content to the beginning of this section
-	 */
-	do_action( 'tribe_events_community_section_before_organizer' );
-	?>
+  <?php
+  /**
+   * Allow developers to hook and add content to the beginning of this section
+   */
+  do_action( 'tribe_events_community_section_before_organizer' );
+  ?>
 
-	<table class="tribe-section-content">
-		<colgroup>
-			<col class="tribe-colgroup tribe-colgroup-label">
-			<col class="tribe-colgroup tribe-colgroup-field">
-		</colgroup>
+  <table class="tribe-section-content">
+    <colgroup>
+      <col class="tribe-colgroup tribe-colgroup-label">
+      <col class="tribe-colgroup tribe-colgroup-field">
+    </colgroup>
 
-		<?php
-		tribe_community_events_organizer_select_menu( $event );
+    <?php
+    tribe_community_events_organizer_select_menu( $event );
 
-		// The organizer meta box will render everything within a <tbody>
-		$organizer_meta_box = new Tribe__Events__Linked_Posts__Chooser_Meta_Box( $event, Tribe__Events__Organizer::POSTTYPE );
-		$organizer_meta_box->render();
-		?>
-	</table>
+    // The organizer meta box will render everything within a <tbody>
+    $organizer_meta_box = new Tribe__Events__Linked_Posts__Chooser_Meta_Box( $event, Tribe__Events__Organizer::POSTTYPE );
+    $organizer_meta_box->render();
+    ?>
+  </table>
+  <div class="hidden-field">
+    <input type="text" name="organizer_data" data-error=".organizer_data_error" value="" class="required" style="position:absolute;z-index: -999;">
+    <div class="organizer_data_error"></div>
+  </div>
+  
 
-	<?php
-	/**
-	 * Allow developers to hook and add content to the end of this section
-	 */
-	do_action( 'tribe_events_community_section_after_organizer' );
-	?>
+  <?php
+  /**
+   * Allow developers to hook and add content to the end of this section
+   */
+  do_action( 'tribe_events_community_section_after_organizer' );
+  ?>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+<script>
+jQuery(document).ready(function($){
+
+  if( $('#saved_tribe_organizer').length ) {
+    $('select#saved_tribe_organizer').val('').trigger('change');
+    $(document).on('change','select#saved_tribe_organizer',function(){
+      var opt = $(this).val();
+      if(opt!='-1') {
+        $('input[name="organizer_data"]').val( $(this).val() );
+        $('#organizer_data-error.error').remove();
+      } else {
+        $('input[name="organizer_data"]').val('');
+      }
+    });
+  }
+
+  $(document).on('click','#event_tribe_organizer a.tribe-add-post',function(e){
+    $('input.tribe_organizer-name').addClass('required');
+    $('input.organizer-phone').addClass('required');
+    $('input.organizer-email').addClass('required');
+
+    $('.new-tribe_organizer input').each(function(){
+      $(this).on('keypress',function(){
+        if( $(this).val().trim().replace('/\s+/g','') ) {
+          $('input[name="organizer_data"]').val( $(this).val().trim().replace('/\s+/g','') );
+          $('#organizer_data-error.error').remove();
+        } else {
+          $('input[name="organizer_data"]').val('');
+        }
+      });
+    });
+  });
+
+  $(document).on('click','#event_tribe_organizer a.tribe-delete-this',function(e){
+    var tr1 = $('#event_tribe_organizer tr.saved-linked-post').length;
+    var tr2 = $('#event_tribe_organizer .new-tribe_organizer').length;
+    var opt = $('select#saved_tribe_organizer').val();
+    if(tr1==1 && tr2==0) {
+      $('input[name="organizer_data"]').val('');
+    } 
+  });
+
+  $(".tribe-community-events.form form").validate({
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      var placement = $(element).data('error');
+      if (placement) {
+        $(placement).append(error)
+      } else {
+        error.insertAfter(element);
+      }
+    },
+    submitHandler: function(form) {
+      // if (grecaptcha.getResponse()) {
+      //   alert('This is only a DEMO!');
+      // } else {
+      //   alert('Please confirm reCaptcha to proceed.');
+      // }
+    }
+  });
+});
+</script>
